b6a8261b55d5f8bf09335df70e6a910c657a3c05
Ujjwal Gupta <guptaxujjwal@gmail.com>
2023-02-13 02:53:53 +0530

    sdv

--- starter/assignment-rev.js
+++ starter/assignment-rev.js
@@ -191,12 +191,13 @@ const percentages3 = [];
 let i = 0;
 while (i < populationszx.length) {
   const percentageOfWorld1 = function (populationOneByOne) {
-    const worldPopulationzz = populationOneByOne / worldPopulationzx;
+    const worldPopulationzz = (populationOneByOne / worldPopulationzx) * 100;
     return worldPopulationzz;
   };
   percentageHaiBey = percentageOfWorld1(populationszx[i]);
 
   percentages3.push[percentageHaiBey];
+  i++;
 }
 
 console.log(percentages3);
